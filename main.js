4996da5fb38fa43e7c8c794c5a5c5de05dcaffdd
Fred Valle <phredvalle@gmail.com>
2019-12-17 14:56:05 -0600

    read csv file with neat-csv node module

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
 const ObjToCsv = require('objects-to-csv');
+const neatCsv = require('neat-csv');
 
-const myObj =  [
-    { name: 'Myname', occupation: 'Dev', hobby: 'drums' }, 
-    { name: 'Myname2', occupation: 'Tech', hobby: 'singing' }, 
-    { name: 'Myname3', occupation: 'marketing', hobby: 'reading' }, 
-    { name: 'Myname4', occupation: 'Tech', hobby: 'bball' }, 
-    { name: 'Myname5', occupation: 'CEO', hobby: 'food' }, 
+const fs = require('fs');
 
-];
+// const myObj =  [
+//     { name: 'Myname', occupation: 'Dev', hobby: 'drums' }, 
+//     { name: 'Myname2', occupation: 'Tech', hobby: 'singing' }, 
+//     { name: 'Myname3', occupation: 'marketing', hobby: 'reading' }, 
+//     { name: 'Myname4', occupation: 'Tech', hobby: 'bball' }, 
+//     { name: 'Myname5', occupation: 'CEO', hobby: 'food' }, 
 
-const csv = new ObjToCsv(myObj);
+// ];
 
-csv.toDisk('./list.csv');
+// const csv = new ObjToCsv(myObj);
+// csv.toDisk('./list.csv');
+
+fs.readFile('./list.csv', async(err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(await neatCsv(data));
+}
+)
 
